f5c1e2e8d726ee2503b7b32d4582bfa03b9c1daf
Guilherme Medeiros Mergulhão <gui.mergulhao55@outlook.com>
2021-04-20 18:38:38 -0300

    Classe Pokemon

---
+++ app/src/main/java/com/example/projetoitau/services/model/Pokemon.kt
@@ -0,0 +1,5 @@
+package com.example.projetoitau.services.model
+
+data class Pokemon(var name: String, var id: Int, var shots: ArrayList<String>, var hp: Int,
+                    var attack: Int, var defense: Int, var specialAttack: Int, var specialDefense: Int,
+                    var speed: Integer, var habilities: ArrayList<String>, var type: String)
---
+++ app/src/main/java/com/example/projetoitau/viewmodel/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.projetoitau.viewmodel
+
+class MainViewModel {
+}
